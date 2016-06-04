0adc1cc6fb503adebaf33dcefde758a1686c692c
jotiger <joyossarian@gmail.com>
2016-06-04 21:42:17 +0800

    First commit of CS

---
+++ CSharp6/CSharp6/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Arrays *****");
+            SimpleArrays();
+            Console.ReadLine();                
+        }
+
+        static void SimpleArrays()
+        {
+            Console.WriteLine("=> Simple Array Creation.");
+            int[] myInts = new int[3];
+            myInts[0] = 100;
+            myInts[1] = 200;
+            myInts[2] = 300;
+            foreach (int i in myInts)
+            {
+                Console.WriteLine(i);
+            }
+            string[] booksOnDotNet = new string[100];
+            Console.WriteLine();
+        }
+
+        static void ArrayOfObjects()
+        {
+            Console.WriteLine("=> Array of Objects.");
+            object[] myObjects = new object[4];
+            myObjects[0] = 10;
+            myObjects[1] = false;
+            myObjects[2] = new DateTime(1969, 3, 24);
+            myObjects[3] = "Form & Void";
+
+        }
+    }
+}
---
+++ CSharp6/CSharp6/vec3.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp6
+{
+    class vec3
+    {
+        public vec3() { }
+
+        public vec3(float e0, float e1, float e2) {
+            e[0] = e0;
+            e[1] = e1;
+            e[2] = e2;
+        }
+
+        float x() { return e[0]; }
+        float y() { return e[1]; }
+        float z() { return e[2]; }
+        float r() { return e[0]; }
+        float g() { return e[1]; }
+        float b() { return e[2]; }
+
+
+        float[] e = new float[3];
+    }
+}
